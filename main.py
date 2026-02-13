0f78f874c22f6b920277605ea6619afe3ae27ebb
Harish Vaishnav <harishvaishnav472@gmail.com>
2026-02-13 15:50:04 +0530

    column_transformer

--- main.py
+++ main.py
@@ -14,4 +14,16 @@ from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x,y, train_size=0.2, random_state=42)
 # print(x_train)
 
-print(x_train.shape)
+# print(x_train.shape)
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn. compose import ColumnTransformer
+
+transformer = ColumnTransformer(transformers = [
+    ('tnf1', OrdinalEncoder(categories =[['no','yes']]), ['smoker']),
+    ('tnf2', OneHotEncoder(sparse_output = False, drop = 'first'), ['sex','region'])],
+    remainder='passthrough')
+print(transformer.fit_transform(x_train).shape)
+print(transformer.fit_transform(x_test).shape)
+
+
